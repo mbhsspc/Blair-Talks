c5d0405149b8d715f4fad8ec883f9d685554a34e
eyob-- <tse.eyob@gmail.com>
2014-08-11 13:32:13 -0400

    Put in a main menu
    So instead of the little ugly action bar up in the top, I made a main
    menu from which you can access the specific teacher you want. I would
    prefer if there could be two columns of teachers, but limited memory
    space forced me to only be able to use 1 column. Oh yeah, also, I added
    a .project and a .classpath

--- src/edu/mbhs/blairtalks/SoundPlayer.java
+++ src/edu/mbhs/blairtalks/SoundPlayer.java
@@ -1,23 +1,15 @@
 package edu.mbhs.blairtalks;
 
 
-import android.graphics.Color;
-import android.graphics.Matrix;
-import android.graphics.drawable.ColorDrawable;
-import android.media.MediaPlayer;
-import android.media.MediaPlayer.OnCompletionListener;
-import android.os.Bundle;
 import android.app.ActionBar;
 import android.app.Activity;
-import android.util.Log;
+import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ImageView.ScaleType;
 import android.widget.SpinnerAdapter;
-import android.widget.Toast;
 
 public class SoundPlayer extends Activity {
 	
@@ -35,93 +27,89 @@ public class SoundPlayer extends Activity {
 		SpinnerAdapter teacher = ArrayAdapter.createFromResource(this, R.array.teachers, android.R.layout.simple_spinner_dropdown_item);
 		mp = MediaPlayer.create(SoundPlayer.this,  R.raw.duval);
 		mp2=MediaPlayer.create(SoundPlayer.this, R.raw.rose);
-		getActionBar().setListNavigationCallbacks(teacher, new ActionBar.OnNavigationListener() {
+		
+		int itemPosition = this.getIntent().getIntExtra("POSITION_CLICKED", -1);
+		switch(itemPosition) {
+		case 0:
+			mp.setLooping(false);
+			mp2.setLooping(false);
+			mp = MediaPlayer.create(SoundPlayer.this, R.raw.duval);
+			play.setImageResource(R.drawable.duval);
 			
-			public boolean onNavigationItemSelected(int itemPosition, long itemId) {
-				switch(itemPosition) {
-				case 0:
-					mp.setLooping(false);
-					mp2.setLooping(false);
-					mp = MediaPlayer.create(SoundPlayer.this, R.raw.duval);
-					play.setImageResource(R.drawable.duval);
-					
-					break;
-				case 1:
-					mp.setLooping(false);
-					mp2.setLooping(false);
-					mp = MediaPlayer.create(SoundPlayer.this, R.raw.dvorsky); 
-					play.setImageResource(R.drawable.dvorsky);
-				
-					break;
-				case 2:
-					mp.setLooping(false);
-					mp2.setLooping(false);
-					mp = MediaPlayer.create(SoundPlayer.this, R.raw.fowler); 
-					play.setImageResource(R.drawable.fowler);
-					break;
-				case 3:
-					mp.setLooping(false);
-					mp2.setLooping(false);
-					mp = MediaPlayer.create(SoundPlayer.this, R.raw.giles); 
-					play.setImageResource(R.drawable.giles);
-					break;
-				case 4:
-					mp.setLooping(false);
-					mp2.setLooping(false);
-					mp = MediaPlayer.create(SoundPlayer.this, R.raw.ostrander); 
-					play.setImageResource(R.drawable.ostrander);
-					break;
-				case 5:
-					mp.setLooping(false);
-					mp2.setLooping(false);
-					mp = MediaPlayer.create(SoundPlayer.this, R.raw.pham); 
-					play.setImageResource(R.drawable.pham);
-					break;
-				case 6:
-					mp.setLooping(false);
-					mp2.setLooping(false);
-					mp = MediaPlayer.create(SoundPlayer.this, R.raw.piper); 
-					play.setImageResource(R.drawable.piper);
-					break;
-				case 7:
-					mp.setLooping(false);
-					mp2.setLooping(false);
-					mp = MediaPlayer.create(SoundPlayer.this, R.raw.rose);
-					
-					play.setImageResource(R.drawable.rose);
-					break;
-				case 8:
-					mp.setLooping(false);
-					mp2.setLooping(false);
-					mp = MediaPlayer.create(SoundPlayer.this, R.raw.schafer); 
-					play.setImageResource(R.drawable.schafer);
-					break;
-				case 9:
-					mp.setLooping(false);
-					mp2.setLooping(false);
-					mp = MediaPlayer.create(SoundPlayer.this, R.raw.stein); 
-					play.setImageResource(R.drawable.stein);
-					break;
-				case 10:
-					mp.setLooping(false);
-					mp2.setLooping(false);
-					mp = MediaPlayer.create(SoundPlayer.this, R.raw.street);
-					play.setImageResource(R.drawable.street);
-					break;
-				case 11:
-				//mp.setLooping(true);
-					play.setImageResource(R.drawable.rosestachecopy);
-				mp2 = MediaPlayer.create(SoundPlayer.this, R.raw.rose);
-				mp2.start();
-				mp2.setLooping(true);
-				mp = MediaPlayer.create(SoundPlayer.this, R.raw.pham);
-				mp.start();
-				mp.setLooping(true);
-				}
-				return true;
-			}
+			break;
+		case 1:
+			mp.setLooping(false);
+			mp2.setLooping(false);
+			mp = MediaPlayer.create(SoundPlayer.this, R.raw.dvorsky); 
+			play.setImageResource(R.drawable.dvorsky);
+		
+			break;
+		case 2:
+			mp.setLooping(false);
+			mp2.setLooping(false);
+			mp = MediaPlayer.create(SoundPlayer.this, R.raw.fowler); 
+			play.setImageResource(R.drawable.fowler);
+			break;
+		case 3:
+			mp.setLooping(false);
+			mp2.setLooping(false);
+			mp = MediaPlayer.create(SoundPlayer.this, R.raw.giles); 
+			play.setImageResource(R.drawable.giles);
+			break;
+		case 4:
+			mp.setLooping(false);
+			mp2.setLooping(false);
+			mp = MediaPlayer.create(SoundPlayer.this, R.raw.ostrander); 
+			play.setImageResource(R.drawable.ostrander);
+			break;
+		case 5:
+			mp.setLooping(false);
+			mp2.setLooping(false);
+			mp = MediaPlayer.create(SoundPlayer.this, R.raw.pham); 
+			play.setImageResource(R.drawable.pham);
+			break;
+		case 6:
+			mp.setLooping(false);
+			mp2.setLooping(false);
+			mp = MediaPlayer.create(SoundPlayer.this, R.raw.piper); 
+			play.setImageResource(R.drawable.piper);
+			break;
+		case 7:
+			mp.setLooping(false);
+			mp2.setLooping(false);
+			mp = MediaPlayer.create(SoundPlayer.this, R.raw.rose);
 			
-		});
+			play.setImageResource(R.drawable.rose);
+			break;
+		case 8:
+			mp.setLooping(false);
+			mp2.setLooping(false);
+			mp = MediaPlayer.create(SoundPlayer.this, R.raw.schafer); 
+			play.setImageResource(R.drawable.schafer);
+			break;
+		case 9:
+			mp.setLooping(false);
+			mp2.setLooping(false);
+			mp = MediaPlayer.create(SoundPlayer.this, R.raw.stein); 
+			play.setImageResource(R.drawable.stein);
+			break;
+		case 10:
+			mp.setLooping(false);
+			mp2.setLooping(false);
+			mp = MediaPlayer.create(SoundPlayer.this, R.raw.street);
+			play.setImageResource(R.drawable.street);
+			break;
+		case 11:
+		//mp.setLooping(true);
+			play.setImageResource(R.drawable.rosestachecopy);
+		mp2 = MediaPlayer.create(SoundPlayer.this, R.raw.rose);
+		mp2.start();
+		mp2.setLooping(true);
+		mp = MediaPlayer.create(SoundPlayer.this, R.raw.pham);
+		mp.start();
+		mp.setLooping(true);
+		}
+		
 	//	mp.setOnCompletionListener(new OnCompletionListener() {
 			
 			//public void onCompletion(MediaPlayer mp) {
